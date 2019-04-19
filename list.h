15272eb03fd0c9bcc27c30e4d2b8d970351e0c96
Freedomrong <shangrong_li@yeah.net>
2019-04-19 10:25:10 +0800

    list file

---
+++ list.h
@@ -0,0 +1,13 @@
+#ifdef _LIST_H_
+#define _LIST_H_
+
+typedef int ElementType;        //    定义数据类型,可根据需要进行其他类型定义
+                                //    链表节点的定义
+typedef struct ListNode {
+    ElementType  Element;        //    数据域，存放数据
+    ListNode* Next;        //    指向下一个链表节点
+}Node, *PNode;
+
+void TraverseList(PNode List);
+
+#endif
